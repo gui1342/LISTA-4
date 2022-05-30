263998be4dbbceb604830293dbc89abde0da2024
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-05-30 17:04:44 -0300

    Update QUESTÃO3.c

--- QUESTÃO3.c
+++ QUESTÃO3.c
@@ -4,12 +4,14 @@ Crie um critério para finalização do programa. Utilize ponteiros.*/
 #include <stdio.h>
 
 int main() {
-  float a = 1; int i = 0;
-  float maior, menor;
+  float a = 1, maior, menor; int i = 0;
+  float *p;
+  p = &a;
+  
   
   while( a != 0){
     printf("\nDigite um número: ");
-    scanf("%f", &a);
+    scanf("%f", p);
     
     if(i == 0)
     {
@@ -17,23 +19,26 @@ int main() {
     menor = a;
     i++;
     }
-    if(a == 0)
+    
+    if(*p == 0)
     {
       printf("O programa foi encerrado!");
       break;
     }
-    if(a > maior || (a <= maior && a > menor))
+    
+    if(*p > maior || (*p <= maior && *p > menor))
     {
       maior = a;
       printf("O menor valor digitado até agora é: %f", menor);
     }
     
-    if(a < menor)
+    if(*p < menor)
     {
       menor = a;
       printf("O menor valor digitado até agora é: %f", menor);
     }
-    else if(a == menor)
+    
+    else if(*p == menor)
     {
       printf("O menor valor digitado até agora é: %f", menor);
     }
